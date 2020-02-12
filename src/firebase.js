c310affb6187f59ae1392b7b615b6cb45a4d7023
Kallan Dautriche <kallan.dautriche@gmail.com>
2020-02-12 11:26:59 +0100

    init firebase

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+});
+
+// class firebaseApp {
+//     constructor() {
+//         debugger;
+//         firebase.initializeApp(configFirebase);
+//     }
+// }
+
+export default firebaseApp;
