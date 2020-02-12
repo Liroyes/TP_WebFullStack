f316e33932e3e0fec4075abbd951ee34553f69a0
Kallan Dautriche <kallan.dautriche@gmail.com>
2020-02-12 13:46:12 +0100

    add and provide firebase reactContext

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -12,6 +12,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import FirebaseContext from '../../firebase';
 
 
 const useStyles = makeStyles(theme => ({
@@ -44,74 +45,81 @@ export default function SignIn() {
   }
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            value={user.email}
-            onChange={e => handleChange(e.target.value, 'email')}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={user.pswd}
-            onChange={e => handleChange(e.target.value, 'pswd')}
-          />
-          <FormControlLabel
-            control={
-                <Checkbox 
-                    color="primary" 
-                    checked={user.remember}
-                    onChange={e => handleChange(e.target.checked, 'remember')}
-                />}
-            label="Remember me"
-            />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            {/* <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid> */}
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
+    <FirebaseContext.Consumer>
+        {firebase => {
+            return (
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <div className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                        Sign in
+                        </Typography>
+                        <form className={classes.form} noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                            value={user.email}
+                            onChange={e => handleChange(e.target.value, 'email')}
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            value={user.pswd}
+                            onChange={e => handleChange(e.target.value, 'pswd')}
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox 
+                                    color="primary" 
+                                    checked={user.remember}
+                                    onChange={e => handleChange(e.target.checked, 'remember')}
+                                />}
+                            label="Remember me"
+                            />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            {/* <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                            </Grid> */}
+                            <Grid item>
+                            <Link href="#" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                            </Grid>
+                        </Grid>
+                        </form>
+                    </div>
+                </Container>
+            )
+        }}
+        
+    </FirebaseContext.Consumer>
   );
 }
